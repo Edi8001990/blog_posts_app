c3606362dd7cec55b170ccad42cc93894909792b
Your Name <you@example.com>
2019-08-31 19:35:12 +0100

    Getting working navigation + woking with sass + boostrap initalizing

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-        <h1>Welcome To laravel</h1>
-        <p>Enjoy with the app</p>
+        <div class="jumbotron text-center">
+                <h1>{{$title}}</h1>
+                <p>Enjoy with the app</p>
+                <button type="button" class="btn btn-primary">Login</button>
+                <button type="button" class="btn btn-success">Register</button>
+        </div>
 @endsection
   
