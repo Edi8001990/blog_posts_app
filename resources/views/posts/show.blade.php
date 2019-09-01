6dd51b8ca14457fc4ce4fe7a249e289256f43540
Your Name <you@example.com>
2019-09-01 15:03:07 +0100

    Post editing initialized

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,8 +13,9 @@
                         <p><small>Writen on {{$post->created_at}}<small></p>
                 </li>
     </ul>
-
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-info">Edit</a>
 </div>
+   
 @endsection
 
 
