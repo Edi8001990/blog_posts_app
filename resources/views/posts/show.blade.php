57c060e6afbf1002d442171249edaf0c32739662
Your Name <you@example.com>
2019-09-04 00:18:04 +0100

    Adding security with access control

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,12 +13,20 @@
                         <p><small>Writen on {{$post->created_at}} by {{$post->user->name}}<small></p>
                 </li>
     </ul>
+
+
+{{-- Disable buttons when vising as guest and verify user --}}
+
+    @if(!Auth::guest()) 
+        @if(Auth::user()->id == $post->user_id)
     <a href="/posts/{{$post->id}}/edit" class="btn btn-info">Edit</a>
 
     {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST' , 'class' => 'float-right'])!!}
             {{Form::hidden('_method', 'DELETE')}}
             {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
     {!!Form::close()!!}
+        @endif
+    @endif
 
 </div>
    
