5f41aa3286c1e7bd4821337fd8f0af4ac0b9e932
Your Name <you@example.com>
2019-09-03 22:26:42 +0100

    Create registration + login ,,,,,, add migration with user_id to post table ,,,,, + edit post fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,43 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>{{config('app.name', 'FootballSchedule')}}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        
-    </head>
-    <body>
-        @include('inc.navbar')
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    
+
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    {{-- <div id="app"> --}}
+       
+
+        <main class="py-4">
+            @include('inc.navbar')
+
             <div class="container">
-                @include('inc.messages')
-                @yield('content')
+                        @include('inc.messages')
+                     @yield('content')
             </div>
 
-            
-            <script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
+        </main>
+    {{-- </div> --}}
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="//cdn.ckeditor.com/4.4.7/standard/ckeditor.js"></script>
                 <script>
                     CKEDITOR.replace( 'article-ckeditor' );
                 </script>
-    </body>
+</body>
 </html>
